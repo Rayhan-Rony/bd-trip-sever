0dcf17804fc314f55a0198c1db6da0e09c93a3e7
Rayhan-Rony <jobayerahmedrony@gmail.com>
2025-09-29 03:35:56 +0600

    remove .env

--- index.js
+++ index.js
@@ -26,32 +26,3 @@ connectToDB()
   .catch((error) => {
     console.log(`server not running because of this culprit : ${error}`);
   });
-
-// async function run() {
-//   const db = client.db("bdTrip");
-//   const tourCollection = db.collection("tours");
-//   try {
-//     app.get("/tours", async (req, res) => {
-//       const tours = await tourCollection.find({}).toArray();
-//       res.send(tours);
-//     });
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log(
-//       "Pinged your deployment. You successfully connected to MongoDB!"
-//     );
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     // await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
-// app.get("/", (req, res) => {
-//   res.send("tour is on");
-// });
-// app.listen(PORT, () => {
-//   console.log("server is running");
-// });
