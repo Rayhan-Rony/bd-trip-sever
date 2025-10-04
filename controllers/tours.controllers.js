8c3c76c1ec9f88bb7d840f82244a170c3e17f55d
Rayhan-Rony <jobayerahmedrony@gmail.com>
2025-10-04 19:58:50 +0600

    complete all tours api wwith limit for home

--- controllers/tours.controllers.js
+++ controllers/tours.controllers.js
@@ -3,9 +3,53 @@ const { getCollection } = require("../db");
 
 // get api for single tours
 exports.getAllTours = asyncHandler(async (req, res) => {
+  const sortBy = req.query.sort;
+  const limit = parseInt(req.query.limit);
+  const page = parseInt(req.query.page);
+  console.log(sortBy, limit, page);
+  let sortOption = {};
+  if (sortBy === "Rating") {
+    sortOption = { rating: 1 };
+  }
+  if (sortBy === "Price") {
+    sortOption = { price: 1 };
+  }
+  if (sortBy === "Title") {
+    sortOption = { title: 1 };
+  }
   const collection = getCollection("tours");
-  const tours = await collection.find({}).toArray();
-  res.json(tours);
+  console.log("outsider");
+  if (page) {
+    console.log("Ok");
+    // calculate skip
+    const skip = (page - 1) * limit;
+
+    // get total count for pagination info
+    const total = await collection.countDocuments();
+
+    const tours = await collection
+      .find({})
+      .sort(sortOption)
+      .skip(skip)
+      .limit(limit)
+      .toArray();
+    // send pagination info with data
+    res.json({
+      data: tours,
+      page,
+      totalPages: Math.ceil(total / limit),
+      totalItems: total,
+    });
+  } else {
+    const tours = await collection.find({}).limit(6).toArray();
+    console.log("ok hitted");
+    res.json({
+      data: tours,
+      page: 1,
+      totalPages: 1,
+      totalItems: tours.length,
+    });
+  }
 });
 // POST /api/tours
 exports.createTour = asyncHandler(async (req, res) => {
